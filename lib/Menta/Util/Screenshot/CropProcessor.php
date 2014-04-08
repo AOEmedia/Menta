47ef53e27628266a2da6d9ab2571c80237b063e1
David Robinson <david.robinson@aoemedia.com>
2014-04-08 12:56:18 -0700

    Added ability to crop screenshot into a new file

--- lib/Menta/Util/Screenshot/CropProcessor.php
+++ lib/Menta/Util/Screenshot/CropProcessor.php
@@ -2,17 +2,30 @@
 
 class Menta_Util_Screenshot_CropProcessor extends Menta_Util_Screenshot_AbstractProcessor implements Menta_Util_Screenshot_ProcessorInterface
 {
+    /**
+     * Cropping coordinates
+     *
+     * @var integer
+     */
     protected $x1;
     protected $y1;
     protected $x2;
     protected $y2;
 
-    public function __construct($x1, $y1, $x2, $y2)
+    /**
+     * Filename for new screenshot (if you don't want to overwrite the original)
+     *
+     * @var string
+     */
+    protected $newFileName;
+
+    public function __construct($x1, $y1, $x2, $y2, $newFileName = '')
     {
         $this->x1 = $x1;
         $this->y1 = $y1;
         $this->x2 = $x2;
         $this->y2 = $y2;
+        $this->newFileName = $newFileName;
     }
 
     public function process()
@@ -26,7 +39,8 @@ class Menta_Util_Screenshot_CropProcessor extends Menta_Util_Screenshot_Abstract
             intval($this->y2 - $this->y1),
             intval($this->x1),
             intval($this->y1),
-            escapeshellarg($this->imageFile)
+            escapeshellarg($this->imageFile),
+            escapeshellarg($this->newFileName ? $this->newFileName: $this->imageFile)
         );
 
         exec($command);
